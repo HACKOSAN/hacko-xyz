9735c3a770afdb31cae2e3192caba16e9558ffc5
Hacko <76522226+HACKOSAN@users.noreply.github.com>
2023-01-02 23:19:33 +0300

    done

--- darkmodestuff.js
+++
@@ -1,5 +0,0 @@
-function toggleDarkLight() {
-    let body = document.getElementById("body");
-    let currentClass = body.className;
-    body.className = currentClass == "dark-mode" ? "pride-mode" : "dark-mode";
-  }
